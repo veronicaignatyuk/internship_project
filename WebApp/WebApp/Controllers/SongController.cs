3ca34323e058c3c55a543a369656b0f5d47cf167
Unknown <unknown@unknown.com>
2016-11-15 10:23:00 +0300

    Add Songpage

---
+++ WebApp/WebApp/Controllers/SongController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+using WebApp.Parser;
+
+namespace WebApp.Controllers
+{
+    public class SongController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        public ActionResult Song(int? id)
+        {
+            SuiteСhord suiteChord = db.SuiteСhords.Find(id);
+            suiteChord.Singer = db.Singers.Find(suiteChord.SingerId);
+            return View(suiteChord);
+        }
+
+    }
+}
