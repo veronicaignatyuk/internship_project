4d95f4ade21b680c311b8dee4fcd172da9b5d269
Unknown <unknown@unknown.com>
2016-11-17 12:29:38 +0300

    Add sorting by singer to SingerList

--- WebApp/WebApp/Controllers/HomeController.cs
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -14,11 +14,23 @@ namespace WebApp.Controllers
     {
         ApplicationDbContext db = new ApplicationDbContext();
 
-        public ActionResult Index(int? page)
+        public ActionResult Index(int? page, string sortOrder)
         {
-            int pageSize =10;
+            ViewBag.CurrentSort = sortOrder;
+            int pageSize = 10;
             int pageNumber = (page ?? 1);
-            return View(db.Singers.ToList().ToPagedList(pageNumber, pageSize));
+            var singers = db.Singers.ToList();
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            switch (sortOrder)
+                {
+                    case "name_desc":
+                        singers = singers.OrderByDescending(s => s.Name).ToList();
+                        break;
+                    default:
+                        singers = singers.OrderBy(s => s.Name).ToList();
+                        break;
+                }
+            return View(singers.ToList().ToPagedList(pageNumber, pageSize));
         }
 
         public ActionResult About()
